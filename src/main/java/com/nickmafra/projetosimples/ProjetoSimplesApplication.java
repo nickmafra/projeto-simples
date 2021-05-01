342bf1fa2f9525aaffa75d710897e45e3b20b81a
Nicolas Mafra <nicolas.mafra@fcamara.com.br>
2021-05-01 20:52:03 -0300

    Estrutura básica criada com Spring Initializr
    Usando as dependências:
    - JPA
    - WEB
    - Driver de conexão MySQL

---
+++ src/main/java/com/nickmafra/projetosimples/ProjetoSimplesApplication.java
@@ -0,0 +1,13 @@
+package com.nickmafra.projetosimples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSimplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSimplesApplication.class, args);
+	}
+
+}
